f2e4d100170f65cdbaf3680c1c13a9be967a4824
Ladu-Lumori <ladulumori2@gmail.com>
2025-05-07 18:25:48 +0300

    prod

--- site.js
+++ site.js
@@ -11,6 +11,32 @@ function PageTransitions() {
             this.className += 'active-btn';
         })
     }
+
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+            // remove selected from other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active');
+            })
+            e.target.classList.add('active');
+
+            // hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active');
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+
+    // Toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode');
+    })
 }
 
 PageTransitions();
